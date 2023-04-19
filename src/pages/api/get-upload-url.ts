1e5cd88442ed6be9fa1f710c67bd22dc6f222ee8
moshawa <moshawa0@gmail.com>
2023-04-19 01:03:51 -0400

    change regex

--- src/pages/api/get-upload-url.ts
+++ src/pages/api/get-upload-url.ts
@@ -13,7 +13,7 @@ export default async function handler(
 		projectId: process.env.PROJECT_ID,
 		credentials: {
 			client_email: process.env.CLIENT_EMAIL,
-			private_key: process.env.PRIVATE_KEY!.replace(/\\n/g, "\n"),
+			private_key: process.env.PRIVATE_KEY!.replace(/\n/g, "\n"),
 		},
 	})
 
