757b8533a04a0181c953c1cd62c3596bd1cb2393
moshawa <moshawa0@gmail.com>
2023-04-20 15:55:08 -0400

    made an oops

--- src/components/Dashboard/MainContent.tsx
+++ src/components/Dashboard/MainContent.tsx
@@ -1,5 +1,7 @@
+import ArrowSVG from "../Misc/ArrowSVG"
 import ApplicantCard from "./ApplicantCard"
 import IApplication from "@/types/Application"
+import Link from "next/link"
 
 type Props = {
 	applicants?: IApplication[]
@@ -18,19 +20,13 @@ export default function Content({ applicants }: Props) {
 					) : (
 						<h1 className="">
 							You have no referrals yet.{" "}
-							<a
+							<Link
 								href="/opportunities"
 								className="text-blue-500 inline-flex items-center"
 							>
 								See open opportunities{" "}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 448 512"
-									className="inline ml-1.5 w-2.5 fill-blue-500 group-hover:fill-blue"
-								>
-									<path d="M440.6 273.4c4.7-4.5 7.4-10.8 7.4-17.4s-2.7-12.8-7.4-17.4l-176-168c-9.6-9.2-24.8-8.8-33.9 .8s-8.8 24.8 .8 33.9L364.1 232 24 232c-13.3 0-24 10.7-24 24s10.7 24 24 24l340.1 0L231.4 406.6c-9.6 9.2-9.9 24.3-.8 33.9s24.3 9.9 33.9 .8l176-168z"></path>
-								</svg>
-							</a>
+								<ArrowSVG classes=" fill-blue-500 group-hover:fill-blue" />
+							</Link>
 						</h1>
 					)}
 				</div>
