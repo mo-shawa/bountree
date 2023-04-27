24b181f00ad9b490ffdbbf993b1fd4dfb73f0c75
moshawa <moshawa0@gmail.com>
2023-04-27 10:23:41 -0400

    temp remove generator

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -56,7 +56,7 @@ export default function ProductPage() {
 						></Image>
 					</div>
 				</div>
-				<JobDescriptionGenerator />
+				{/* <JobDescriptionGenerator /> */}
 			</section>
 		</Layout>
 	)
