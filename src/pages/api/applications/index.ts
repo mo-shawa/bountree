73fcd6f2643122084eed5fb432f6674536752d11
moshawa <moshawa0@gmail.com>
2023-04-28 13:31:24 -0400

    minimal sendgrid test success

--- src/pages/api/applications/index.ts
+++ src/pages/api/applications/index.ts
@@ -3,6 +3,7 @@ import serverAuthenticate from "@/utils/serverAuthenticate"
 import IApplication from "@/types/Application"
 import { createApplication } from "../../../../controllers/application"
 import { getOpportunityById } from "../../../../controllers/opportunity"
+import sgMail from "@sendgrid/mail"
 
 export default async function handler(
 	req: NextApiRequest,
@@ -14,6 +15,23 @@ export default async function handler(
 	try {
 		if (method === "POST") {
 			const parsedBody = JSON.parse(req.body)
+
+			sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
+			const msg = {
+				to: "mahmoud@shawa.dev", // Change to your recipient
+				from: "ms@bountree.app", // Change to your verified sender
+				subject: "Sending with SendGrid is Fun",
+				text: "and easy to do anywhere, even with Node.js",
+				html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+			}
+			sgMail
+				.send(msg)
+				.then(() => {
+					console.log("Email sent")
+				})
+				.catch((error) => {
+					console.error(error)
+				})
 
 			const application: IApplication = {
 				...parsedBody,
