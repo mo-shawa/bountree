7a29af4b0f536d01cb6e909f97cc39e162053112
moshawa <moshawa0@gmail.com>
2023-05-01 14:59:44 -0400

    protect dashboard

--- src/pages/api/users/[id]/dashboard.ts
+++ src/pages/api/users/[id]/dashboard.ts
@@ -1,10 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { getApplicationsByUser } from "../../../../../controllers/application"
+import serverAuthenticate from "@/utils/serverAuthenticate"
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
+	const { token } = await serverAuthenticate(req, res)
+	if (!token) {
+		return res.status(401).json({ error: "Unauthorized" })
+	}
+
 	const {
 		query: { id },
 	} = req
