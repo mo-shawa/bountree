551e2506a8e2428fa6f193ea38cee29441f88c94
moshawa <moshawa0@gmail.com>
2023-05-07 21:03:58 -0400

    concurrent connection test

--- db/connect.ts
+++ db/connect.ts
@@ -1,28 +1,61 @@
-import { Db, MongoClient, MongoClientOptions } from "mongodb"
+import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb"
 
-if (!process.env.DATABASE_URI) {
-	throw new Error('Invalid/Missing environment variable: "DATABASE_URL"')
-}
-
-const uri: string = process.env.DATABASE_URI
-
+const uri = process.env.DATABASE_URI as string // your mongodb connection string
 const options: MongoClientOptions = {
-	maxPoolSize: 2,
-	maxIdleTimeMS: 10000,
+	maxPoolSize: 1,
+	maxIdleTimeMS: 5000,
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 }
 
-let client: MongoClient
-let clientPromise: Promise<MongoClient>
+class Singleton {
+	private static _instance: Singleton
+	private client: MongoClient
+	private clientPromise: Promise<MongoClient>
+	private constructor() {
+		this.client = new MongoClient(uri, options)
+		this.clientPromise = this.client.connect()
+		if (process.env.NODE_ENV === "development") {
+			// In development mode, use a global variable so that the value
+			// is preserved across module reloads caused by HMR (Hot Module Replacement).
+			global._mongoClientPromise = this.clientPromise
+		}
 
-if (process.env.NODE_ENV === "development") {
-	if (!global._mongoClientPromise) {
-		client = new MongoClient(uri, options)
-		global._mongoClientPromise = client.connect()
+		this.client.addListener("close", () => {
+			console.log("MongoDB connection closed")
+		})
+		this.client.addListener("reconnect", () => {
+			console.log("MongoDB reconnected")
+		})
+		this.client.addListener("timeout", () => {
+			console.log("MongoDB connection timed out")
+		})
+		this.client.addListener("error", (err) => {
+			console.log("MongoDB error", err)
+		})
+		this.client.addListener("connectionPoolClosed", () => {
+			console.log("MongoDB connection pool closed")
+		})
+		this.client.addListener("connectionPoolCreated", () => {
+			console.log("MongoDB connection pool created")
+		})
+		this.client.addListener("connectionCreated", () => {
+			console.log("MongoDB connection created")
+		})
 	}
-	clientPromise = global._mongoClientPromise
-} else {
-	client = new MongoClient(uri, options)
-	clientPromise = client.connect()
-}
 
+	public static get instance() {
+		if (!this._instance) {
+			this._instance = new Singleton()
+		}
+		return this._instance.clientPromise
+	}
+}
+const clientPromise = Singleton.instance
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
 export default clientPromise
