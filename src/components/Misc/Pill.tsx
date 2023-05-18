d39ec04420afb483df100bb3ada732bf3acd5eb0
moshawa <moshawa0@gmail.com>
2023-05-17 22:32:25 -0400

    potential badge

--- src/components/Misc/Pill.tsx
+++ src/components/Misc/Pill.tsx
@@ -3,14 +3,39 @@ import { classNames } from "@/utils/misc"
 type PillProps = {
 	children: React.ReactNode
 	className?: string
-	type?: "blue" | "green" | "red" | "yellow"
+	type?: PillColor
 }
 
-const pillColors = {
-	blue: "bg-blue-300 text-blue-800 border border-blue-800",
-	green: "bg-green-300 text-green-800 border border-green-800 ",
-	red: "bg-red-300 text-red-800 border border-red-800 ",
-	yellow: "bg-yellow-300 text-yellow-800 border border-yellow-800 ",
+type PillColor =
+	| "red"
+	| "orange"
+	| "yellow"
+	| "green"
+	| "blue"
+	| "indigo"
+	| "purple"
+	| "pink"
+	| "gray"
+
+type PillColorMap = {
+	[key in PillColor]: string
+}
+
+const pillColors: PillColorMap = {
+	blue: "bg-blue-300 text-blue-800 border border-blue-800 shadow-md shadow-blue-300",
+	green:
+		"bg-green-300 text-green-800 border border-green-800 shadow-md shadow-green-300 ",
+	red: "bg-red-300 text-red-800 border border-red-800 shadow-md shadow-red-300 ",
+	yellow:
+		"bg-yellow-300 text-yellow-800 border border-yellow-800 shadow-md shadow-yellow-300 ",
+	orange:
+		"bg-orange-300 text-orange-800 border border-orange-800 shadow-md shadow-orange-300 ",
+	indigo:
+		"bg-indigo-300 text-indigo-800 border border-indigo-800 shadow-md shadow-indigo-300 ",
+	purple:
+		"bg-purple-300 text-purple-800 border border-purple-800 shadow-md shadow-purple-300 ",
+	pink: "bg-pink-300 text-pink-800 border border-pink-800 shadow-md shadow-pink-300 ",
+	gray: "bg-gray-300 text-gray-800 border border-gray-800 shadow-md shadow-gray-300  ",
 }
 
 export default function Pill({
--- src/components/Opportunities/MappedOpportunities.tsx
+++ src/components/Opportunities/MappedOpportunities.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
-import OpportunityCard from './OpportunityCard'
-import Filter from './Filter'
-import IOpportunity from '@/types/opportunity'
-import { formatCurrency } from '@/utils/misc'
+import { useState, useEffect } from "react"
+import OpportunityCard from "./OpportunityCard"
+import Filter from "./Filter"
+import IOpportunity from "@/types/opportunity"
+import { formatCurrency } from "@/utils/misc"
 
 type OpportunitiesProps = {
 	data: IOpportunity[]
@@ -10,16 +10,16 @@ type OpportunitiesProps = {
 
 export default function MappedOpportunities({ data }: OpportunitiesProps) {
 	const [filteredData, setFilteredData] = useState<IOpportunity[]>()
-	const [filteredBy, setFilteredBy] = useState<string>('All roles')
+	const [filteredBy, setFilteredBy] = useState<string>("All roles")
 
 	useEffect(() => {
 		setFilteredData(data)
 	}, [data])
 
 	useEffect(() => {
-		if (filteredBy === 'All roles') setFilteredData(data)
+		if (filteredBy === "All roles") setFilteredData(data)
 
-		if (filteredBy !== 'All roles') {
+		if (filteredBy !== "All roles") {
 			const dataCopy = structuredClone(data)
 
 			const filteredData = dataCopy.filter(
@@ -35,31 +35,10 @@ export default function MappedOpportunities({ data }: OpportunitiesProps) {
 		<>
 			<section className="bg-b-blue-dark">
 				<div className="mx-auto px-4 w-full max-w-7xl">
-					<Filter
-						filteredBy={filteredBy}
-						setFilteredBy={setFilteredBy}
-					/>
+					<Filter filteredBy={filteredBy} setFilteredBy={setFilteredBy} />
 					{filteredData && filteredData.length ? (
-						filteredData.map((item) => (
-							<OpportunityCard
-								id={item._id}
-								key={item._id}
-								company={item.company.name}
-								image={item.company.image}
-								slogan={item.company.slogan}
-								role={{
-									title: item.title,
-									category: item.category,
-									workFrom: item.remote ? 'Remote' : 'In person',
-									location: item.location,
-									reward: formatCurrency(
-										item.reward.amount,
-										item.reward.currency
-									),
-								}}
-								salary={item.salary}
-								status={item.status}
-							/>
+						filteredData.map((opportunity) => (
+							<OpportunityCard opportunity={opportunity} />
 						))
 					) : (
 						<div className="w-full bg-white rounded-md flex flex-row justify-between items-center p-4 my-4">
