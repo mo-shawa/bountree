5d70a18a38f4601aafd83653888922c8f4592cb4
Mahmoud El-Shawa <moshawa0@gmail.com>
2023-05-24 15:51:47 +0400

    working admin applications view

--- controllers/application.ts
+++ controllers/application.ts
@@ -25,39 +25,40 @@ export async function createApplication(application: IApplication) {
 }
 
 export async function getAdminApplications() {
-	const client = await clientPromise
-	const db = client.db(process.env.DATABASE_NAME)
-	const applications = await db
-		.collection('applications')
-		.aggregate([
-			{
-				$lookup: {
-					from: 'opportunities',
-					localField: 'opportunityId',
-					foreignField: '_id',
-					as: 'opportunity',
-				},
-			},
-			{
-				$unwind: '$opportunity',
-			},
-			{
-				$lookup: {
-					from: 'users',
-					localField: 'userId',
-					foreignField: '_id',
-					as: 'user',
-				},
-			},
-			{
-				$unwind: {
-					path: '$user',
-				},
-			},
-		])
-		.sort({ createdAt: -1 })
-		.toArray()
+	const applications = await constructApplicationsWithOpportunities()
 	return applications
+
+	// const applications = await db
+	// 	.collection('applications')
+	// 	.aggregate([
+	// 		{
+	// 			$lookup: {
+	// 				from: 'opportunities',
+	// 				localField: 'opportunityId',
+	// 				foreignField: '_id',
+	// 				as: 'opportunity',
+	// 			},
+	// 		},
+	// 		{
+	// 			$unwind: '$opportunity',
+	// 		},
+	// 		{
+	// 			$lookup: {
+	// 				from: 'users',
+	// 				localField: 'userId',
+	// 				foreignField: '_id',
+	// 				as: 'user',
+	// 			},
+	// 		},
+	// 		{
+	// 			$unwind: {
+	// 				path: '$user',
+	// 			},
+	// 		},
+	// 	])
+	// 	.sort({ createdAt: -1 })
+	// 	.toArray()
+	// return applications
 }
 
 export async function updateApplicationStatus(
@@ -86,50 +87,7 @@ export async function updateApplicationStatus(
 }
 
 export async function getApplicationsByUser(id: string) {
-	const userApplications = await firestore
-		.collection('applications')
-		.where('userId', '==', id)
-		.get()
-
-	const applicationData = userApplications.docs.map((doc) => ({
-		...doc.data(),
-		id: doc.id,
-		opportunityId: doc.data().opportunityId,
-		createdAt: doc.data().createdAt.toMillis(),
-		updatedAt: doc.data().updatedAt.toMillis(),
-	}))
-	const opportunityIds = Array.from(
-		new Set(applicationData.map((application) => application.opportunityId))
-	)
-
-	const opportunities = await firestore
-		.collection('opportunities')
-		.where(FieldPath.documentId(), 'in', opportunityIds)
-		.get()
-
-	const opportunityData = opportunities.docs.map((doc) => ({
-		...doc.data(),
-		id: doc.id,
-		createdAt: doc.data().createdAt.toMillis(),
-		updatedAt: doc.data().updatedAt.toMillis(),
-	}))
-
-	const applications = applicationData.map((application) => ({
-		...application,
-		opportunity: opportunityData.find(
-			(opportunity) => opportunity.id === application.opportunityId
-		),
-	}))
-
-	console.log({
-		applicationData,
-		opportunityIds,
-		opportunities,
-		opportunityData,
-		applications,
-	})
-
-	return applications
+	return constructApplicationsWithOpportunities(id)
 }
 
 export async function checkDuplicateApplicant(
@@ -148,4 +106,67 @@ export async function checkDuplicateApplicant(
 	})
 
 	return foundCandidate
+}
+
+async function constructApplicationsWithOpportunities(id?: string) {
+	const userApplications = id
+		? await firestore
+				.collection('applications')
+				.where('userId', '==', id)
+				.orderBy('createdAt', 'desc')
+				.get()
+		: await firestore
+				.collection('applications')
+				.orderBy('createdAt', 'desc')
+				.get()
+
+	const applicationData = userApplications.docs.map((doc) => ({
+		...doc.data(),
+		id: doc.id,
+		opportunityId: doc.data().opportunityId,
+		userId: doc.data().userId,
+		createdAt: doc.data().createdAt.toMillis(),
+		updatedAt: doc.data().updatedAt.toMillis(),
+	}))
+	const opportunityIds = Array.from(
+		new Set(applicationData.map((application) => application.opportunityId))
+	)
+
+	const userIds = Array.from(
+		new Set(applicationData.map((application) => application.userId))
+	)
+
+	const users = await firestore
+		.collection('users')
+		.where(FieldPath.documentId(), 'in', userIds)
+		.get()
+
+	const userData = users.docs.map((doc) => ({
+		...doc.data(),
+		id: doc.id,
+		createdAt: doc.data().createdAt.toMillis(),
+		updatedAt: doc.data().updatedAt.toMillis(),
+	}))
+
+	const opportunities = await firestore
+		.collection('opportunities')
+		.where(FieldPath.documentId(), 'in', opportunityIds)
+		.get()
+
+	const opportunityData = opportunities.docs.map((doc) => ({
+		...doc.data(),
+		id: doc.id,
+		createdAt: doc.data().createdAt.toMillis(),
+		updatedAt: doc.data().updatedAt.toMillis(),
+	}))
+
+	const applications = applicationData.map((application) => ({
+		...application,
+		opportunity: opportunityData.find(
+			(opportunity) => opportunity.id === application.opportunityId
+		),
+		user: userData.find((user) => user.id === application.userId),
+	}))
+
+	return applications
 }
